5330cef24ce39cef057fc4d6fb9d7ed87cac4396
technologic <20627603+technologic808@users.noreply.github.com>
2020-04-19 18:50:16 +0530

    Add apps to connect page

--- src/app/connect-page/connect-page.component.ts
+++ src/app/connect-page/connect-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faGithubSquare,
+  faLinkedin,
+  faSteamSquare,
+  faInstagramSquare,
+  faYoutubeSquare,
+} from '@fortawesome/free-brands-svg-icons';
+
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-connect-page',
   templateUrl: './connect-page.component.html',
-  styleUrls: ['./connect-page.component.css']
+  styleUrls: ['./connect-page.component.css'],
 })
 export class ConnectPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  faGithubSquare = faGithubSquare;
+  faLinkedin = faLinkedin;
+  faSteamSquare = faSteamSquare;
+  faInstagramSquare = faInstagramSquare;
+  faYoutubeSquare = faYoutubeSquare;
+  faEnvelopeSquare = faEnvelopeSquare;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
